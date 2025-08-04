3b061cbe24cb63b2e311c23ce70675c3f8d0f83d
Amelie Ngan <86581293+aimsngn@users.noreply.github.com>
2025-08-04 05:00:35 +0000

    pages + database

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,6 @@
-$( document ).ready(function() {
-    
-    var envelope = $('#envelope');
-    var btn_open = $("#open");
-    var btn_reset = $("#reset");
-    
-    envelope.click( function() {
-        open();
-    });
-    btn_open.click( function() {
-        open();
-    });
-    btn_reset.click( function() {
-        close();
-    });
-
-    function open() {
-        envelope.addClass("open")
-           .removeClass("close");
-    }
-    function close() {
-        envelope.addClass("close")
-           .removeClass("open");
-    }
-   
+$(document).ready(function () {
+  const envelope = $('#envelope');
+  envelope.click(function () {
+    envelope.toggleClass('open close');
+  });
 });
